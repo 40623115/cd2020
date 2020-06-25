94f9a7edeb971275c5cb87a735dea287ba800918
40623115 <40623115@gm.nfu.edu.tw>
2020-06-25 11:27:43 +0800

    add reveal week3 and upload jpg

--- 作業1.py
+++ 作業1.py
@@ -1,22 +1,21 @@
 import random
  
-list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; # 分組名單
+list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; 
 
-random.shuffle(list) #打亂名單
+random.shuffle(list) 
 
-print("打亂後的名單=",list) #顯示打亂後的名單
+print("????",list) 
 
-X = 5 #分為幾組
+X = 5 
 
-Y=int(len(list)/X) #計算多少人為一組
+Y=int(len(list)/X) 
 
 list_2 = [] 
 
-for i in range(0,len(list),Y): #執行for迴圈 i ，將名單循環列出 
+for i in range(0,len(list),Y): 
     
-    list_2.append(list[i:i+Y]) #依序排列分組人數
+    list_2.append(list[i:i+Y]) 
 
     
-print("分組名單=",list_2) #顯示結果
-
+print("????",list_2) 
 
